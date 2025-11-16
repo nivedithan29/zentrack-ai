5b04f84486c455073382c1cec8b7a90f1d0a5f3f
Niveditha N <163818215+nivedithan29@users.noreply.github.com>
2025-11-16 16:21:33 +0530

    Create MemoryBank class for data storage

---
+++ src/memory/memory_bank.py
@@ -0,0 +1,10 @@
+# Memory Bank (placeholder)
+class MemoryBank:
+    def __init__(self):
+        self.data = {}
+
+    def save(self, key, value):
+        self.data[key] = value
+
+    def load(self, key):
+        return self.data.get(key)
